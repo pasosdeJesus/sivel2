750e3c1009cf55d123ae2c2b4bcc6f81e35f67a9
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-09-04 07:31:19 -0500

    anexos en heroku

--- config/initializers/sivel2_gen.rb
+++ config/initializers/sivel2_gen.rb
@@ -1,4 +1,8 @@
 Sivel2Gen.setup do |config|
       config.ruta_anexos = "/var/www/resbase/anexos"
+      # En heroku los anexos son super-temporales
+      if !ENV["HEROKU_POSTGRESQL_MAUVE_URL"].nil?
+        config.ruta_anexos = "#{Rails.root}/tmp/"
+      end
       config.titulo = "SIVeL " + Sivel2Gen::VERSION
 end
