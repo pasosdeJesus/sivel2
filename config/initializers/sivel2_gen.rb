a46bfba559966bd9cf5f0828b91e339574d9b320
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-09-10 09:19:54 -0500

    heroku

--- config/initializers/sivel2_gen.rb
+++ config/initializers/sivel2_gen.rb
@@ -3,7 +3,7 @@ Sivel2Gen.setup do |config|
       config.ruta_volcados = "/var/www/resbase/sivel2_sjrven/"
       # En heroku los anexos son super-temporales
       if ENV["HEROKU_POSTGRESQL_MAUVE_URL"]
-        Sivel2Gen.config.ruta_anexos = "#{Rails.root}/tmp/"
+        config.ruta_anexos = "#{Rails.root}/tmp/"
       end
       config.titulo = "SIVeL " + Sivel2Gen::VERSION
 end
