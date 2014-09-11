ca69b565a5439d5ba8129f616e307b00d912f647
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-09-11 11:40:19 -0500

    mejora bin/gc.sh

--- bin/gc.sh
+++ bin/gc.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-# Hace prueba de regresión y envia a github
+# Hace pruebas, pruebas de regresión y envia a github
+
+grep "^ *gem *.sivel2_gen. *, *path:" Gemfile > /dev/null 2> /dev/null
+if (test "$?" = "0") then {
+	echo "Gemfile inlcuye un sivel2_gen cableado al sistema de archivos"
+	exit 1;
+} fi;
 
 rspec
 if (test "$?" = "0") then {
