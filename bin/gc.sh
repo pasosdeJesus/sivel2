dbe782bfe1573d177558722df46541845ca236d0
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-09-17 05:31:51 -0500

    Actualiza

--- bin/gc.sh
+++ bin/gc.sh
@@ -3,18 +3,35 @@
 
 grep "^ *gem *.sivel2_gen. *, *path:" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
-	echo "Gemfile inlcuye un sivel2_gen cableado al sistema de archivos"
+	echo "Gemfile incluye un sivel2_gen cableado al sistema de archivos"
+	exit 1;
+} fi;
+grep "^ *gem *.debugger*" Gemfile > /dev/null 2> /dev/null
+if (test "$?" = "0") then {
+	echo "Gemfile incluye debugger que heroku no quiere"
+	exit 1;
+} fi;
+
+RAILS_ENV=test rake db:drop db:setup sivel2:indices
+if (test "$?" != "0") then {
+	echo "No puede preparse base de prueba";
 	exit 1;
 } fi;
 
 rspec
-if (test "$?" = "0") then {
-	b=`git branch | grep "^*" | sed -e  "s/^* //g"`
-	git status -s
-	git commit -a
-	git push origin ${b}
-	if (test "$?" = "0") then {
-		git push heroku master
-	} fi;
+if (test "$?" != "0") then {
+	echo "No pasaron pruebas";
+	exit 1;
 } fi;
+
+b=`git branch | grep "^*" | sed -e  "s/^* //g"`
+git status -s
+git commit -a
+git push origin ${b}
+if (test "$?" != "0") then {
+	echo "No pudo subirse el cambio a github";
+	exit 1;
+} fi;
+
+git push heroku master
 
