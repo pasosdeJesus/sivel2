de7f263cec5e71c3e657812f73497804aa54b5ad
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-12-06 12:15:37 -0500

    Actualiza con rake rails:update

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,13 +14,15 @@ module Sivel2
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Bogota'
+    config.time_zone = 'America/Bogota'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.time_zone = 'America/Bogota'
     config.i18n.default_locale = :es
-    config.active_record.schema_format = :sql
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.active_record.schema_format = :sql
   end
 end
