22bd0c9c299dd8c8f3a8cb8234f2384f457e8e02
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-01-04 06:45:58 -0500

    Inusable pruebas con javascript

--- test/integration/casoconjs_test.rb
+++ test/integration/casoconjs_test.rb
@@ -21,20 +21,99 @@ class CasoconjsTest < Capybara::Rails::TestCase
     # Datos básicos
     fill_in "Fecha del Hecho", with: '2014-08-05'
     fill_in "Titulo", with: 'titulo'
+
+    click_on "Víctimas"
+    if (!find_link('Añadir Víctima').visible?)
+      click_on "Víctimas"
+    end
+    click_on "Añadir Víctima"
+    within ("div#victima") do 
+      fill_in "Nombres", with: 'Nombres V'
+      fill_in "Apellidos", with: 'Apellidos V'
+      select("1999", from: 'Año de nacimiento')
+      select("ENERO", from: 'Mes de nacimiento')
+      select("1", from: 'Día de nacimiento')
+      select("MASCULINO", from: 'Sexo')
+      select("CÉDULA DE CIUDADANÍA", from: 'Tipo de Documento')
+      fill_in "Número Documento", with: '19222'
+      select('ALBANIA', from: 'País de Nacionalidad')
+      select('RUSIA', from: 'País de Nacimiento')
+      select('OTRO', from: 'Profesión')
+      select('ROM', from: 'Etnia') 
+      select('IGLESIA DE DIOS', from: 'Religión/Iglesia') 
+      select('HETEROSEXUAL', from: 'Orientación Sexual') 
+    end
+
+    click_link "Ubicación"
+    if (!find_link('Añadir Ubicación').visible?)
+      click_link "Ubicación"
+    end
+    assert page.has_content?("Añadir Ubicación")
+    click_on "Añadir Ubicación"
+    if (!page.has_content?('Latitud'))
+      click_on "Añadir Ubicación"
+    end
+    within ("div#ubicacion") do 
+      assert page.has_select?('País')
+      select('VENEZUELA', from: 'País') 
+      assert page.has_select?('País', selected: 'VENEZUELA')
+      puts "Eligió país"
+      assert page.has_select?('Estado/Departamento', with_options: ['ARAGUA'])
+      puts "Lleno con AJAX departamento"
+      # Capybara+poltergeist+phantomjs 1.9.8 no permiten lo siguiente
+      if false
+        # La siguiente impide Guardar con error Internal Server Error
+        select('ARAGUA', from: 'Estado/Departamento') 
+        puts "Eligió departamento"
+        page.save_screenshot('/tmp/s2-traselegirdepto')
+        puts "Esperando Municipio con AJAX"
+        # Los siguientes bloquean o presentan error 
+        rc = page.evaluate_script("$.active").to_i
+        puts "rc=#{rc}"
+        while  rc>0
+          rc = page.evaluate_script("$.active").to_i
+          puts "rc=#{rc}"
+        end
+        puts "Esperando Municipio con AJAX 2"
+        assert page.has_select?('Municipio', with_options: ['CAMATAGUA'])
+        puts "Lleno con AJAX municipio"
+        select('CAMATAGUA', from: 'Municipio') 
+        puts "Eligió municipio"
+        assert page.has_select?('Centro Poblado', with_options: ['CARMEN DE CURA'])
+        puts "Lleno con AJAX Centro Poblado"
+        select('CARMEN DE CURA', from: 'Centro Poblado') 
+        puts "Eligió Centro Poblado"
+      end
+      fill_in "Lugar", with: 'Lugar'
+      fill_in "Sitio", with: 'Sitio'
+      fill_in "Latitud", with: '4.1'
+      fill_in "Longitud", with: '-74.3'
+      select('URBANO', from: 'Tipo de Sitio') 
+    end
+    click_on "Añadir Ubicación"
+    su = "//div[@id='ubicacion']/div[2]"
+    within(:xpath, su) do 
+      select('COLOMBIA', from: 'País') 
+      assert page.has_select?('Estado/Departamento', with_options: ['BOYACÁ'])
+      rc = page.evaluate_script("$.active").to_i
+      puts "hay depto rc=#{rc}"
+      if false
+        select('BOYACÁ', from: 'Estado/Departamento') 
+        select('CHISCAS', from: 'Municipio') 
+        select('CHISCAS', from: 'Centro Poblado') 
+      end
+      fill_in "Lugar", with: 'Lugar2'
+      fill_in "Sitio", with: 'Sitio2'
+      fill_in "Latitud", with: '4.2'
+      fill_in "Longitud", with: '-74.32'
+      select('RURAL', from: 'Tipo de Sitio') 
+    end
+
+    puts "Guardando"
     click_button "Guardar"
+    puts page.html
     assert page.has_content?("2014-08-05")
 
-    # Solicitante Principal
-    click_on "Editar"
-    fill_in "Hora", with: '3:00 PM'
-    fill_in "Duracion", with: '2'
-    select("ANTIOQUIA CHOCO SANT", from: 'Región')
-    select("Ecuador", from: 'Frontera')
-    click_button "Guardar"
-    assert page.has_content?("2014-08-05")
-    #puts page.body
-    # Driver no acepta: accept_confirm do click_on "Eliminar" end
-    #expect(page).to have_content("Casos")
   end
 
 end
