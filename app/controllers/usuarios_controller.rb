c4adb2348ad9468b36430b70fc9d38733886d2b6
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-09-16 05:17:17 -0500

    usa sip

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 
-require 'sivel2_gen/concerns/controllers/usuarios_controller'
+require 'sip/concerns/controllers/usuarios_controller'
 
 class UsuariosController < Sip::ModelosController
     include Sip::Concerns::Controllers::UsuariosController
