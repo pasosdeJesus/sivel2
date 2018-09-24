200016fc77295c5a64c423150726f00508a87e7a
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2018-09-24 08:37:53 -0500

    corre

--- bin/corre.sh
+++ bin/corre.sh
@@ -8,7 +8,7 @@ if (test "$RC" = "") then {
 	export RC=sivel2
 } fi;
 if (test "$RAILS_ENV" = "development") then {
-	bin/rails -s -p $PUERTODES
+	bin/rails s -p $PUERTODES -b $IPDES
 } else {
 	if (test ! -f /etc/rc.d/$RC) then {
 		echo "Falta script /etc/rc.d/$RC"
