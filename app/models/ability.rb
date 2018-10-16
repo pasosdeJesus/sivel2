caf99b47590b705380687f01ace047bb50329f34
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2018-10-16 11:42:42 -0500

    Incluye inicio de reporte consolidad de victimas

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -27,37 +27,51 @@ class Ability  < Sivel2Gen::Ability
     if !usuario || usuario.fechadeshabilitacion
       return
     end
+    
+    can :descarga_anexo, Sip::Anexo
     can :contar, Sip::Ubicacion
+    can :nuevo, Sip::Ubicacion
+
     can :contar, Sivel2Gen::Caso
     can :buscar, Sivel2Gen::Caso
     can :lista, Sivel2Gen::Caso
-    can :descarga_anexo, Sip::Anexo
-    can :nuevo, Sip::Ubicacion
+    can :nuevo, Sivel2Gen::Combatiente
     can :nuevo, Sivel2Gen::Presponsable
     can :nuevo, Sivel2Gen::Victima
     can :nuevo, Sivel2Gen::Victimacolectiva
-    can :nuevo, Sivel2Gen::Combatiente
+
     if usuario && usuario.rol then
       case usuario.rol 
       when Ability::ROLANALI
+        can :read, Heb412Gen::Doc
+        can :read, Heb412Gen::Plantilladoc
+        can :read, Heb412Gen::Plantillahcm
+        can :read, Heb412Gen::Plantillahcr
+
+        can :manage, Sip::Persona
+
+        can :manage, Sivel2Gen::Acto
+        can :manage, Sivel2Gen::Actocolectivo
         can :read, Sivel2Gen::Caso
         can :new, Sivel2Gen::Caso
         can :nuevo, Sivel2Gen::Caso
         can [:update, :create, :destroy], Sivel2Gen::Caso
-        can :manage, Sip::Persona
-        can :manage, Sivel2Gen::Acto
-        can :manage, Sivel2Gen::Actocolectivo
-        can :read, Heb412Gen::Doc
-        can :read, Heb412Gen::Plantillahcm
+        can :read, Sivel2Gen::Victima
       when Ability::ROLADMIN
-        can :manage, Sivel2Gen::Caso
+        can :manage, Heb412Gen::Doc
+        can :manage, Heb412Gen::Plantilladoc
+        can :manage, Heb412Gen::Plantillahcm
+        can :manage, Heb412Gen::Plantillahcr
+
         can :manage, Sip::Persona
-        can :manage, Usuario
+        can :manage, Sip::Respaldo7z
+
         can :manage, Sivel2Gen::Acto
         can :manage, Sivel2Gen::Actocolectivo
-        can :manage, Heb412Gen::Doc
-        can :manage, Heb412Gen::Plantillahcm
-        can :manage, Sip::Respaldo7z
+        can :manage, Sivel2Gen::Caso
+        can :read, Sivel2Gen::Victima
+
+        can :manage, Usuario
         can :manage, :tablasbasicas
         tablasbasicas.each do |t|
           c = Ability.tb_clase(t)
