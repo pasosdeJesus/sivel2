c2adbe5465fe2c362c10c2aa7411bb0f56dedbaa
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2018-12-03 10:38:00 -0500

    Mejora migracion

--- db/migrate/20170928100402_plantilla_ejemplo.rb
+++ db/migrate/20170928100402_plantilla_ejemplo.rb
@@ -8,6 +8,9 @@ class PlantillaEjemplo < ActiveRecord::Migration[5.1]
           licencia, vista, nombremenu, filainicial FROM heb412_gen_plantillahcm
           WHERE id<100
         );
+      UPDATE heb412_gen_campoplantillahcm SET 
+        plantillahcm_id=plantillahcm_id+100 WHERE plantillahcm_id<100;
+      DELETE FROM heb412_gen_plantillahcm WHERE id<100;
 
       SELECT setval('heb412_gen_campoplantillahcm_id_seq', MAX(id))
         FROM (SELECT 1000 as id UNION 
