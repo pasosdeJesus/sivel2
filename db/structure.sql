b11c6c188ea5707d44d470f08a14b5e912c3e124
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-02-26 07:32:21 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -4380,7 +4380,7 @@ ALTER TABLE ONLY public.sivel2_gen_victimacolectiva
 -- PostgreSQL database dump complete
 --
 
-SET search_path TO public, pg_catalog;
+SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
 ('20131128151014'),
