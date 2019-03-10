7f5693ab896fc4b9924aeb6c5e9826ec8f2f61e4
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-03-10 07:23:41 -0500

    Mejor no usa variable de entorno RAILS_RELATIVE_URL_ROOT

--- bin/u.sh
+++ bin/u.sh
@@ -12,19 +12,17 @@ if (test "${DIRAP}" = "") then {
 	echo "Definir directorio de la aplicación en DIRAP"
 	exit 1;
 } fi;
-#if (test "${RAILS_RELATIVE_URL_ROOT}" = "") then {
-#	echo "Definir ruta relativa en URL en RAILS_RELATIVE_URL_ROOT"
-#	exit 1;
-#} fi;
+
+defuroot=""
+if (test "${RAILS_RELATIVE_URL_ROOT}" != "") then {
+	defuroot="RAILS_RELATIVE_URL_ROOT=${RAILS_RELATIVE_URL_ROOT}"
+} fi;
 
 DOAS=`which doas 2> /dev/null`
 if (test "$?" != "0") then {
 	DOAS="sudo"
 } fi;
-$DOAS su ${USUARIO_AP} -c "cd $DIRAP; RAILS_ENV=production RAILS_RELATIVE_URL_ROOT=${RAILS_RELATIVE_URL_ROOT} bin/rails assets:precompile"
-$DOAS su ${USUARIO_AP} -c "cd $DIRAP; RAILS_RELATIVE_URL_ROOT=${RAILS_RELATIVE_URL_ROOT} RAILS_ENV=production bin/rails sip:indices"
-$DOAS su ${USUARIO_AP} -c "cd $DIRAP; echo \"Iniciando unicorn...\"; RAILS_RELATIVE_URL_ROOT=${RAILS_RELATIVE_URL_ROOT} RAILS_ENV=production SECRET_KEY_BASE=${SECRET_KEY_BASE} bundle exec unicorn_rails -c $DIRAP/config/unicorn.conf.minimal.rb  -E production -D"
-
-
-  
+$DOAS su ${USUARIO_AP} -c "cd $DIRAP; RAILS_ENV=production ${defuroot} bin/rails assets:precompile"
+$DOAS su ${USUARIO_AP} -c "cd $DIRAP; ${defuroot} RAILS_ENV=production bin/rails sip:indices"
+$DOAS su ${USUARIO_AP} -c "cd $DIRAP; echo \"Iniciando unicorn...\"; ${defuroot} RAILS_ENV=production SECRET_KEY_BASE=${SECRET_KEY_BASE} bundle exec unicorn_rails -c $DIRAP/config/unicorn.conf.minimal.rb  -E production -D"
 
