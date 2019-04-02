d39d0e43c4b957100da32349dccd26d8268bda15
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-04-01 22:20:51 -0500

    Actualiza a sip que actualiza DIVIPOLA, ver https://github.com/pasosdeJesus/sip/wiki/Actualizaci%C3%B3n-a-Divipola-2018 y usa esquema, ver https://github.com/pasosdeJesus/sip/wiki/Actualizaci%C3%B3n-a-PostgreSQL-posterior-a-10.2

--- bin/gc.sh
+++ bin/gc.sh
@@ -39,7 +39,7 @@ if (test "$SINMIG" != "1") then {
 	} fi;
 } fi;
 
-RAILS_ENV=test bundle exec rake db:drop db:setup; RAILS_ENV=test bundle exec rake db:migrate sip:indices
+RAILS_ENV=test bundle exec rake db:drop db:setup; RAILS_ENV=test bin/rails db:migrate sip:indices
 if (test "$?" != "0") then {
 	echo "No puede preparse base de prueba";
 	exit 1;
