35a45097a465720c2f156e2d7d08f509c872985f
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-05-31 18:45:27 -0500

    Refina pruebas al sistema

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -9,6 +9,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
   driven_by :poltergeist, screen_size: [1400, 1400], options: { 
     js_errors: true,
+    timeout: 2.minutes,
     logger: NilLogger.new#,
 #    phantomjs_logger: STDOUT,
 #    phantomjs_options: ['--debug=true'],
