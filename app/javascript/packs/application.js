968774ccf5f528385839fe76841016f66ac28cf5
alejocruzrcc <alejocruzzz@gmail.com>
2019-09-12 15:57:28 -0500

    organización webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,5 +14,5 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
+var L = require('leaflet')
 console.log('Hello World from Webpacker')
