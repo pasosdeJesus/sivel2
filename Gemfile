e82430239f5266ec016d42fb613d3540d149aa65
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2020-11-01 12:31:28 -0500

    mapa tiempo

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ gem 'rails', '~> 6.1'
 
 gem 'rails-i18n'
 
+gem 'react-rails'
+
 gem 'redcarpet' # Markdown
 
 gem 'rspreadsheet' # Genera ODS
---
+++ app/packs/entrypoints/infomapa.jsx
@@ -0,0 +1,11 @@
+// Mapa con casos e indicadores, fuentes de Covid19.health
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
+
+import App from '@pasosdejesus/mapa_tiempo_yi_liu'
+
+render(<App/>, document.getElementById('root'))
+
+
