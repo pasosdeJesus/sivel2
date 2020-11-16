1aa1640f634ee822bde04b7c8c6028b2772528fc
Bayron Ordoñez <55851241+Bayron3094@users.noreply.github.com>
2020-11-16 16:00:43 -0500

    Sivel2.1 (#33)
    * Cambio de ruta a /mapatiempo
    
    * Implementacion de la nueva version de mapa_tiempo_yi_liu con mi version (Bayron3094) en la rama bubble_plot
    
    * actualiza
    
    * Usa nuevo motor apo214 de desaparicion
    
    * Actualiza
    
    * Cambio de ruta a /mapatiempo
    
    * actualiza
    
    * Usa nuevo motor apo214 de desaparicion
    
    * Actualiza
    
    * Aumenta ruta
    
    * Usa nuevo motor apo214 de desaparicion
    
    * Actualiza plantilla y gemas
    
    * Update package.json
    
    Co-authored-by: Vladimir Támara Patiño <vtamara@pasosdeJesus.org>

--- app/javascript/packs/mapatiempo.jsx
+++ app/javascript/packs/mapatiempo.jsx
@@ -4,7 +4,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { render } from 'react-dom'
 
+<<<<<<< HEAD
 import MapaTiempo from '@pasosdejesus/mapa_tiempo_yi_liu'
 
 render(<MapaTiempo casos_url='https://base.nocheyniebla.org/casos/cuenta' usar_proxy_cors='false' />, document.getElementById('root'))
+=======
+import App from '@pasosdejesus/mapa_tiempo_yi_liu'
+
+render(<App/>, document.getElementById('root'))
+
+>>>>>>> 4377efb... Sivel2.1 (#33)
 
