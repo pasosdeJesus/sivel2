06baf3f7da1da703f6a39466d70aac6fb434ca9a
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2020-11-27 17:49:54 -0500

    usa nuevo nombre de componente MapaTiempo

--- app/javascript/packs/mapatiempo.jsx
+++ app/javascript/packs/mapatiempo.jsx
@@ -4,14 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { render } from 'react-dom'
 
-<<<<<<< HEAD
 import MapaTiempo from '@pasosdejesus/mapa_tiempo_yi_liu'
 
 render(<MapaTiempo casos_url='https://base.nocheyniebla.org/casos/cuenta' usar_proxy_cors='false' />, document.getElementById('root'))
-=======
-import App from '@pasosdejesus/mapa_tiempo_yi_liu'
-
-render(<App/>, document.getElementById('root'))
->>>>>>> af2069a... Sivel2.1 (#33)
-
 
