47ca5f46777a84f6c8d4fd1cded7ff7f6ee5c56b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2020-12-01 05:55:46 -0500

    Emplea cambios a usuario en sivel2_gen

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,56 +1,9 @@
 # encoding: UTF-8
 
-require 'sip/concerns/controllers/usuarios_controller'
+require 'sivel2_gen/concerns/controllers/usuarios_controller'
 
 class UsuariosController < Heb412Gen::ModelosController
-  include Sip::Concerns::Controllers::UsuariosController
-
-  def vistas_manejadas
-    ['Usuario']
-  end
-
-  def atributos_index
-    r = [ 
-      :id,
-      :nusuario,
-      :nombre,
-      :descripcion,
-      :rol
-    ]
-    if can?(:manage, Sip::Grupo)
-      r += [:sip_grupo]
-    end
-    r += [ 
-      :email,
-      :tema,
-      :created_at_localizada,
-      :habilitado
-    ]
-    r
-  end
-
-  def atributos_form
-    r = [ 
-      :nusuario,
-      :nombre,
-      :descripcion,
-      :rol
-    ]
-    if can?(:manage, Sip::Grupo)
-      r += [:sip_grupo]
-    end
-    r += [
-      :email,
-      :tema,
-      :idioma,
-      :encrypted_password,
-      :fechacreacion_localizada,
-      :fechadeshabilitacion_localizada,
-      :failed_attempts,
-      :unlock_token,
-      :locked_at
-    ]
-  end
+  include Sivel2Gen::Concerns::Controllers::UsuariosController
 
 end
 
