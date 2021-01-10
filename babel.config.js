202457dcdbb4982fd2e20957d401a01ad86bce28
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-01-10 07:51:21 -0500

    Devuelve a rails 6.0 con suavidad

--- babel.config.js
+++ babel.config.js
@@ -22,10 +22,8 @@ module.exports = function(api) {
         {
           targets: {
             node: 'current'
-          },
-          modules: 'commonjs'
-        },
-        '@babel/preset-react'
+          }
+        }
       ],
       (isProductionEnv || isDevelopmentEnv) && [
         '@babel/preset-env',
@@ -35,13 +33,6 @@ module.exports = function(api) {
           corejs: 3,
           modules: false,
           exclude: ['transform-typeof-symbol']
-        }
-      ],
-      [
-        '@babel/preset-react',
-        {
-          development: isDevelopmentEnv || isTestEnv,
-          useBuiltIns: true
         }
       ]
     ].filter(Boolean),
