855a880353c075b130c9ebeea7f18c5dd764dd33
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-02-04 14:07:09 -0500

    Actualiza para operar con ruby 3 y nuevas variables de arranque

--- config/routes.rb
+++ config/routes.rb
@@ -45,5 +45,6 @@ Rails.application.routes.draw do
   mount Mr519Gen::Engine, at: rutarel, as: 'mr519_gen'
   mount Heb412Gen::Engine, at: rutarel, as: 'heb412_gen'
   mount Sivel2Gen::Engine, at: rutarel, as: 'sivel2_gen'
+  mount Apo214::Engine, at: rutarel, as: 'apo214'
 
 end
