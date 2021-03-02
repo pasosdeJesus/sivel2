7235a6d6ddbe1bceef57a5a3db17706d8ef82f81
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-03-02 15:51:05 -0500

    Pie de consulta web

--- config/initializers/punto_montaje.rb
+++ config/initializers/punto_montaje.rb
@@ -1,4 +1,5 @@
 Sivel2::Application.config.relative_url_root = ENV.fetch(
   'RUTA_RELATIVA', '/sivel2')
 Sivel2::Application.config.assets.prefix = ENV.fetch(
-  'RUTA_RELATIVA', '/sivel2') + '/assets'
+  'RUTA_RELATIVA', '/sivel2') == '/' ? 
+ '/assets' : (ENV.fetch('RUTA_RELATIVA', '/sivel2') + '/assets')
