d72d4507f73da957036f5d9cd2dde36b3a9022a6
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-03-02 06:21:45 -0500

    Titulo en español

--- bin/u.sh
+++ bin/u.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Arranca con unicorn --suponiendo que ya se ejecutaron otras labores
 #   necesarias para ejecutar como instalar gemas, generar recursos, 
-#   actualizar indices, etc.  Ver bin/corre
+#   actualizar índices, etc.  Ver bin/corre
 
 if (test "${DIRAP}" = "") then {
   echo "Definir directorio de la aplicación en DIRAP"
@@ -35,23 +35,23 @@ if (test "$?" != "0") then {
 $DOAS su - ${USUARIO_AP} -c "cd $DIRAP; 
   echo \"== Iniciando unicorn... ==\"; 
   ${defuroot} BD_CLAVE='${BD_CLAVE}' \
-    BD_PRO='${BD_PRO}' \
-    BD_USUARIO='${BD_USUARIO}' \
-    CONFIG_HOSTS='${CONFIG_HOSTS}' \
-    DIRAP='${DIRAP}' \
-    HEB412_RUTA='${HEB412_RUTA}' \
-    PUERTOUNICORN='${PUERTOUNICORN}' \
+    BD_PRO=\"${BD_PRO}\" \
+    BD_USUARIO=\"${BD_USUARIO}\" \
+    CONFIG_HOSTS=\"${CONFIG_HOSTS}\" \
+    DIRAP=\"${DIRAP}\" \
+    HEB412_RUTA=\"${HEB412_RUTA}\" \
+    PUERTOUNICORN=\"${PUERTOUNICORN}\" \
     RAILS_ENV=production \
-    RUTA_RELATIVA='${RUTA_RELATIVA}' \
-    SECRET_KEY_BASE='${SECRET_KEY_BASE}' \
-    SIP_FORMATO_FECHA='${SIP_FORMATO_FECHA}' \
-    SIP_RUTA_ANEXOS='${SIP_RUTA_ANEXOS}' \
-    SIP_RUTA_VOLCADOS='${SIP_RUTA_VOLCADOS}' \
-    SIP_TITULO='${SIP_TITULO}' \
-    SIVEL2_CONSWEB_MAX='${SIVEL2_CONSWEB_MAX}' \
-    SIVEL2_CONSWEB_EPILOGO='${SIVEL2_CONSWEB_EPILOGO}' \
-    SIVEL2_CONSWEB_PUBLICA='${SIVEL2_CONSWEB_PUBLICA}' \
-    SIVEL2_MAPAOSM_DIASATRAS='${SIVEL2_MAPAOSM_DIASATRAS}' \
+    RUTA_RELATIVA=\"${RUTA_RELATIVA}\" \
+    SECRET_KEY_BASE=\"${SECRET_KEY_BASE}\" \
+    SIP_FORMATO_FECHA=\"${SIP_FORMATO_FECHA}\" \
+    SIP_RUTA_ANEXOS=\"${SIP_RUTA_ANEXOS}\" \
+    SIP_RUTA_VOLCADOS=\"${SIP_RUTA_VOLCADOS}\" \
+    SIP_TITULO=\"${SIP_TITULO}\" \
+    SIVEL2_CONSWEB_MAX=\"${SIVEL2_CONSWEB_MAX}\" \
+    SIVEL2_CONSWEB_EPILOGO=\"${SIVEL2_CONSWEB_EPILOGO}\" \
+    SIVEL2_CONSWEB_PUBLICA=\"${SIVEL2_CONSWEB_PUBLICA}\" \
+    SIVEL2_MAPAOSM_DIASATRAS=\"${SIVEL2_MAPAOSM_DIASATRAS}\" \
     bundle exec /usr/local/bin/unicorn_rails \
       -c ${DIRAP}/config/unicorn.conf.minimal.rb  -E production -D"
 
