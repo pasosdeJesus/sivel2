bf6d5cec8f71be8edc1c49d3913a6d1306395532
alejocruzrcc <alejocruzzz@gmail.com>
2021-04-06 06:36:18 -0500

    cambia a espanol (#43)

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -20,8 +20,8 @@ export default class extends Controller {
   end(event) {
     let id = event.item.dataset.id
     let data = new FormData()
-    data.append("nuevaposition", event.newIndex + 1)
-    data.append("viejaposition", event.oldIndex + 1)
+    data.append("nuevaposicion", event.newIndex + 1)
+    data.append("viejaposicion", event.oldIndex + 1)
     Rails.ajax({
       url: this.data.get("url").replace(":id", id),
       type: 'PATCH',
