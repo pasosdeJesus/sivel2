540fe8ccd0cd2f7521ea569e579a24ee47220f62
alejocruzrcc <alejocruzzz@gmail.com>
2021-05-13 01:21:29 -0500

    funciona active storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-  config.relative_url_root = "/sivel2"
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
