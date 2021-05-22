98c573faac50e16ed01a6b349fa0d4f4b0fd5837
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-05-22 14:22:00 -0500

    Actualiza webpacker y expose-loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,23 +1,22 @@
 const { environment } = require('@rails/webpacker')
-
 const webpack = require('webpack')
 
 environment.plugins.prepend(
-    'Provide',
-    new webpack.ProvidePlugin({
-         $: 'jquery',
-          jQuery: 'jquery',
-          jquery: 'jquery',
-          Popper: ['popper.js', 'default'],
-        })
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
 )
 
-environment.loaders.append('expose', {
-      test: require.resolve('jquery'),
-      use: [
-                  { loader: 'expose-loader', options: '$' },
-                  { loader: 'expose-loader', options: 'jQuery' },
-                ]
+environment.loaders.append('jquery', {
+  test: require.resolve('jquery'),
+  loader: 'expose-loader',
+  options: {
+    exposes: ['$', 'jQuery']
+  }
 })
 
 module.exports = environment
