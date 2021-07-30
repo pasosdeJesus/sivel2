bbf9b38dcded6e8f7d0694920e0da26ae9de5b9a
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-07-30 04:48:37 -0500

    Update index.js

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,5 +1,5 @@
-// Load all the channels within this directory and all subdirectories.
-// Channel files must be named *_channel.js.
+// Carga todos los canales de este directorio y sus subdirectorios.
+// Los archivos con canales deben ser de la forma  *_channel.js.
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
