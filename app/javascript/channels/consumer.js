926b4019e118a5bde33f6da62168e9ae77265468
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-07-30 04:47:41 -0500

    Update consumer.js

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,5 +1,5 @@
-// Action Cable provides the framework to deal with WebSockets in Rails.
-// You can generate new channels where WebSocket features live using the `bin/rails generate channel` command.
+// ActionCable provee el marco de trabajo para manejar WebSockets en Rails.
+// Puede generar nuevos canales donde viven las características de WebSocket usando la orden `bin/rails generate channel`
 
 import { createConsumer } from "@rails/actioncable"
 
