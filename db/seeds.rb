15df1df19e204f556c402ded352888fa54ed9cc3
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-08-29 20:16:33 -0500

    Actualiza

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ end
 
 # usuario sivel2 con clave sivel2
 conexion.execute("INSERT INTO public.usuario 
-	(nusuario, email, encrypted_password, password, 
+	(nusuario, nombre, email, encrypted_password, password, 
   fechacreacion, created_at, updated_at, rol) 
-	VALUES ('sivel2', 'sivel2@localhost', 
+	VALUES ('sivel2', 'sivel2', 'sivel2@localhost', 
 	'$2a$10$V2zgaN1ED44UyLy0ubey/.1erdjHYJusmPZnXLyIaHUpJKIATC1nG', 
 	'', '2014-08-26', '2014-08-26', '2014-08-26', 1);")
 
