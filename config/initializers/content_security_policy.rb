1e2f461b1ac61afdad34322f534909b70fc84229
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-10-26 16:33:14 -0500

    mapa descargable

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,12 +5,12 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
  Rails.application.config.content_security_policy do |policy|
-   policy.default_src :self, :https
-   policy.font_src    :self, :https, :data
-   policy.img_src     :self, :https, :data
+#   policy.default_src :self, :https
+#   policy.font_src    :self, :https, :data
+#   policy.img_src     :self, :https, :data
    policy.object_src  :none
-   policy.script_src  :self, :https
-   policy.style_src   :self, :https
+#   policy.script_src  :self, :https
+#   policy.style_src   :self, :https
    policy.frame_ancestors :self, 'https://www.nocheyniebla.org'
    # If you are using webpack-dev-server then specify webpack-dev-server host
 #   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
