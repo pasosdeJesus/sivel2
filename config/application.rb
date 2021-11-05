3f898d191023e8adc53c7283363f281b5a874697
Nombre Usuario <correo@usuario.net>
2021-11-05 12:26:20 -0500

    csp

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,14 @@ module Sivel2
     config.relative_url_root = ENV.fetch('RUTA_RELATIVA', "/sivel2")
 
     config.action_dispatch.default_headers = {
-      'X-Frame-Options' => 'ALLOW'
+      #'X-Frame-Options' => 'ALLOW'
+      #'X-Frame-Options' => 'SAMEORIGIN',
+      'X-Frame-Options' => 'https://www.nocheyniebla.org',
+      'X-XSS-Protection' => '1; mode=block',
+      'X-Content-Type-Options' => 'nosniff',
+      'X-Download-Options' => 'noopen',
+      'X-Permitted-Cross-Domain-Policies' => 'none',
+      'Referrer-Policy' => 'strict-origin-when-cross-origin'
     }
 
     config.x.origen_cors = ENV.fetch('ORIGEN_CORS','').split(',')
