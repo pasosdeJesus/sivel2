feebf18c4547a8006491a624346c4bd11e85aca7
alejocruzrcc <alejocruzzz@gmail.com>
2022-01-13 08:56:45 -0500

    R579 (#71)
    * Actualiza
    
    * Actualiza
    
    * docuemnta y prueba mr519gen
    
    * mr519

---
+++ test/controllers/mr519_gen/control_acceso_campos_controller_test.rb
@@ -0,0 +1,88 @@
+require 'test_helper'
+require 'nokogiri'
+
+module Mr519Gen
+  class ControlAccesoCamposControllerTest < ActionDispatch::IntegrationTest
+
+    include Rails.application.routes.url_helpers
+    include Devise::Test::IntegrationHelpers
+
+    setup  do
+      if ENV['CONFIG_HOSTS'] != 'www.example.com'
+        raise 'CONFIG_HOSTS debe ser www.example.com'
+      end
+      @formulario = Mr519Gen::Formulario.create!(PRUEBA_FORMULARIO)
+    end
+
+    PRUEBA_FORMULARIO = {
+      nombre: "formu ejemplo",
+      nombreinterno: "formueje"
+    }
+
+    PRUEBA_CAMPO = {
+      nombre: "n",
+      nombreinterno: "nn"
+    }
+
+    # No autenticado
+    ################
+
+    test "sin autenticar no debe acceder a campos/new" do
+      assert_raise CanCan::AccessDenied do
+        get mr519_gen.new_campo_path
+      end
+    end
+
+    test "sin autenticar no debe eliminar campo" do
+      skip
+      @campo = Mr519Gen::Campo.create!(PRUEBA_CAMPO.merge(formulario_id: @formulario.id))
+      assert_raise CanCan::AccessDenied do
+        delete mr519_gen.campo_path(@campo.id)
+      end
+    end
+
+
+    # Autenticado como operador sin grupo
+    #####################################
+
+    test "observador no debe acceder a campos/new" do
+      current_usuario = ::Usuario.find(PRUEBA_USUARIO_OP)
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        get mr519_gen.new_campo_path
+      end
+    end
+
+    test "observador no debe eliminar campo" do
+      skip
+      current_usuario = ::Usuario.find(PRUEBA_USUARIO_OP)
+      sign_in current_usuario
+      @campo = Mr519Gen::Campo.create!(PRUEBA_CAMPO.merge(formulario_id: @formulario.id))
+      assert_raise CanCan::AccessDenied do
+        delete mr519_gen.campo_path(@campo.id)
+      end
+    end
+
+
+    # Autenticado como operador con grupo Analista de Casos
+    #######################################################
+    test "analista no debe acceder a campos/new" do
+      current_usuario = ::Usuario.find(PRUEBA_USUARIO_AN)
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        get mr519_gen.new_campo_path
+      end
+    end
+
+    test "analista no debe eliminar campo" do
+      skip
+      current_usuario = ::Usuario.find(PRUEBA_USUARIO_AN)
+      sign_in current_usuario
+      @campo = Mr519Gen::Campo.create!(PRUEBA_CAMPO.merge(formulario_id: @formulario.id))
+      assert_raise CanCan::AccessDenied do
+        delete mr519_gen.campo_path(@campo.id)
+      end
+    end
+
+  end
+end
---
+++ test/controllers/mr519_gen/control_acceso_planesencuesta_controller_test.rb
@@ -0,0 +1,226 @@
+require 'test_helper'
+require 'nokogiri'
+
+module Mr519Gen
+  class ControlAccesoPlanesencuestaControllerTest < ActionDispatch::IntegrationTest
+
+    include Rails.application.routes.url_helpers
+    include Devise::Test::IntegrationHelpers
+
+    setup  do
+      if ENV['CONFIG_HOSTS'] != 'www.example.com'
+        raise 'CONFIG_HOSTS debe ser www.example.com'
+      end
+      @planencuesta = Mr519Gen::Planencuesta.create!(PRUEBA_PLANENCUESTA)
+    end
+
+    PRUEBA_PLANENCUESTA = {
+      id: 10,
+      campo_id: 1,
+      valor: "eje",
+      respuestafor_id: 2
+    }
+
+    # No autenticado
+    ################
+
+    test "sin autenticar no debe listar planesencuesta" do
+      assert_raise CanCan::AccessDenied do
+        get mr519_gen.planesencuesta_path
+      end
+    end
+
+    test "sin autenticar no debe ver registro de un planencuesta" do
+      assert_raise CanCan::AccessDenied do
+        get mr519_gen.planencuesta_path(@planencuesta.id)
+      end
+    end
+
+    test "sin autenticar no debe ver vista de editar un planencuesta" do
+      assert_raise CanCan::AccessDenied do
+        get mr519_gen.edit_planencuesta_path(@planencuesta.id)
+      end
+    end
+
+    test "sin autenticar no debe actualizar put un planencuesta" do
+      assert_raise CanCan::AccessDenied do
+        put mr519_gen.planencuesta_path(@planencuesta.id)
+      end
+    end
+
+    test "sin autenticar no debe actualizar patch  un planencuesta" do
+      assert_raise CanCan::AccessDenied do
+        patch mr519_gen.planencuesta_path(@planencuesta.id)
+      end
+    end
+
+    test "sin autenticar no debe eliminar un planencuesta" do
+      assert_raise CanCan::AccessDenied do
+        delete mr519_gen.planencuesta_path(@planencuesta.id)
+      end
+    end
+
+    test "sin autenticar no debe postear planesencuesta" do
+      assert_raise CanCan::AccessDenied do
+        post mr519_gen.planesencuesta_path, params: {
+          planencuesta: PRUEBA_PLANENCUESTA
+        }
+      end
+    end
+
+    test "sin autenticar no debe ver planencuesta de nuevo planencuesta" do
+      assert_raise CanCan::AccessDenied do
+        get mr519_gen.new_planencuesta_path()
+      end
+    end
+
+
+
+    # Autenticado como operador sin grupo
+    #####################################
+
+    test "autenticado como operador sin grupo no debe presentar listado de planesencuesta" do
+      current_usuario = Usuario.create!(PRUEBA_USUARIO_OP)
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        get mr519_gen.planesencuesta_path
+      end
+    end
+
+    test "observador no debe postear planesencuesta" do
+      current_usuario = Usuario.create!(PRUEBA_USUARIO_OP)
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        post mr519_gen.planesencuesta_path, params: {
+          planencuesta: PRUEBA_PLANENCUESTA
+        }
+      end
+    end
+
+
+    test "autenticado como operador sin grupo puede ver planencuesta de nuevo planencuesta" do
+      current_usuario = Usuario.create!(PRUEBA_USUARIO_OP)
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        get mr519_gen.new_planencuesta_path()
+      end
+    end
+
+    test "observador no debe ver registro de un planencuesta" do
+      current_usuario = Usuario.create!(PRUEBA_USUARIO_OP)
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        get mr519_gen.planencuesta_path(@planencuesta.id)
+      end
+    end
+
+    test "observador no debe ver vista de editar un planencuesta" do
+      current_usuario = Usuario.create!(PRUEBA_USUARIO_OP)
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        get mr519_gen.edit_planencuesta_path(@planencuesta.id)
+      end
+    end
+
+    test "observador no debe actualizar put un planencuesta" do
+      current_usuario = Usuario.create!(PRUEBA_USUARIO_OP)
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        put mr519_gen.planencuesta_path(@planencuesta.id)
+      end
+    end
+
+    test "observador no debe actualizar patch  un planencuesta" do
+      current_usuario = Usuario.create!(PRUEBA_USUARIO_OP)
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        patch mr519_gen.planencuesta_path(@planencuesta.id)
+      end
+    end
+
+    test "observador no debe eliminar un planencuesta" do
+      current_usuario = Usuario.create!(PRUEBA_USUARIO_OP)
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        delete mr519_gen.planencuesta_path(@planencuesta.id)
+      end
+    end
+
+    # Autenticado como operador con grupo Analista de Casos
+    #######################################################
+
+    def inicia_analista
+      current_usuario = Usuario.create!(PRUEBA_USUARIO_AN)
+      current_usuario.sip_grupo_ids = [20]
+      current_usuario.save
+      return current_usuario
+    end
+
+    test "autenticado como operador analista no debe presentar listado de planesencuesta" do
+      current_usuario = inicia_analista
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        get mr519_gen.planesencuesta_path
+      end
+    end
+
+    test "operador analista no debe postear planesencuesta" do
+      current_usuario = inicia_analista
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        post mr519_gen.planesencuesta_path, params: {
+          planencuesta: PRUEBA_PLANENCUESTA
+        }
+      end
+    end
+
+    test "autenticado como operador analista no debe presentar planencuesta de nuevo planencuesta" do
+      current_usuario = inicia_analista
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        get mr519_gen.new_planencuesta_path()
+      end
+    end
+
+    test "operador analista no debe ver registro de un planencuesta" do
+      current_usuario = inicia_analista
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        get mr519_gen.planencuesta_path(@planencuesta.id)
+      end
+    end
+
+    test "operador analista no debe ver vista de editar un planencuesta" do
+      current_usuario = inicia_analista
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        get mr519_gen.edit_planencuesta_path(@planencuesta.id)
+      end
+    end
+
+    test "operador analista no debe actualizar put un planencuesta" do
+      current_usuario = inicia_analista
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        put mr519_gen.planencuesta_path(@planencuesta.id)
+      end
+    end
+
+    test "operador analista no debe actualizar patch  un planencuesta" do
+      current_usuario = inicia_analista
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        patch mr519_gen.planencuesta_path(@planencuesta.id)
+      end
+    end
+
+    test "operador analista no debe eliminar un planencuesta" do
+      current_usuario = inicia_analista
+      sign_in current_usuario
+      assert_raise CanCan::AccessDenied do
+        delete mr519_gen.planencuesta_path(@planencuesta.id)
+      end
+    end
+
+  end
+end
