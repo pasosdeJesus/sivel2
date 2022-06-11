9c2f68faad5e630b0e71d32187865c7cbbbd2978
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-06-11 06:17:04 -0500

    Créditos y Acerca de

--- config/initializers/sip.rb
+++ config/initializers/sip.rb
@@ -10,4 +10,26 @@ Sip.setup do |config|
     config.ruta_anexos = "#{Rails.root}/tmp/"
   end
   config.titulo = "SIVeL #{Sivel2Gen::VERSION}"
+
+  config.descripcion = "Sistema de Información de Violencia Política en Línea"
+  config.codigofuente = "https://github.com/pasosdeJesus/sivel2"
+  config.urlcontribuyentes = "https://github.com/pasosdeJesus/sivel2/graphs/contributors"
+  config.urlcreditos = "https://github.com/pasosdeJesus/sivel2/blob/main/CREDITOS.md"
+  config.agradecimientoDios = "<p>
+El mayor agradecimiento al Dios trino, el de la Biblia, a quien dedicamos 
+este trabajo y a quien oramos para que no sea usado por estructuras armadas
+sino para oponernos a la violencia y a la corrupción y para darle honra a Él.
+</p>
+<blockquote>
+SEÑOR, tú escucharás las oraciones de la gente humilde
+y le darás ánimo a su corazón; préstales atención.
+Protege a los indefensos, haz justicia a los pobres y oprimidos,
+y que el ser humano no cause más violencia sobre la tierra.
+Cantaré a Jehová, <br>
+Porque me ha hecho bien.
+<br>
+Salmo 13:6
+</blockquote>
+".html_safe
+
 end
