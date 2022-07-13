94552716a751ae9eebc777cb1820b8158b30e76e
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-07-13 05:22:49 -0500

    Guion para eliminar casos sin memos a partir de una fecha. Closes #767

---
+++ scripts/elimina_sin_memo_tras_fecha.rb
@@ -0,0 +1,15 @@
+# Ejecutar con 
+#bin/rails runner -e development scripts/elimina_sin_memo_tras_fecha.rb  2022-01-01
+
+require_relative 'auxiliar_eliminar'
+
+puts ARGV
+fechaini = ARGV[0]
+if fechaini.nil? 
+  puts "Primer parametro debe ser fecha desde la cual eliminar sin memo y no '#{fechaini}'"
+  exit 1
+end
+fechaini = Sip::FormatoFechaHelper.reconoce_adivinando_locale(fechaini)
+
+eliminar_casos("SELECT id FROM sivel2_gen_caso WHERE fecha>='#{fechaini}' AND TRIM(memo)=''");
+
