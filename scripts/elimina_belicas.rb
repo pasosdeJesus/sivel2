6285a85a2bfef20fc52fd4dc49e630169dd9d6b5
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-09-14 15:30:49 -0500

    Mejora eliminacion de belicas

--- scripts/elimina_belicas.rb
+++ scripts/elimina_belicas.rb
@@ -1,74 +1,28 @@
 # Ejecutar con 
 # bin/rails runner -e development scripts/elimina_belicas.rb 
 
-ActiveRecord::Base.connection.execute <<-SQL
-  DELETE FROM sivel2_gen_antecedente_combatiente;
-  DELETE FROM combatiente_presponsable;
-  DELETE FROM combatiente;
-  DELETE FROM sivel2_gen_caso_categoria_presponsable WHERE id_categoria IN 
-    (SELECT c.id FROM  sivel2_gen_categoria AS c JOIN 
-    sivel2_gen_supracategoria AS s ON c.supracategoria_id=s.id WHERE 
-    id_tviolencia='C');
-  DELETE FROM sivel2_gen_actocolectivo WHERE id_categoria IN (SELECT c.id 
-    FROM  sivel2_gen_categoria AS c JOIN sivel2_gen_supracategoria AS s ON 
-    c.supracategoria_id=s.id WHERE id_tviolencia='C');
-  DELETE FROM sivel2_gen_acto WHERE id_categoria IN (SELECT c.id 
-    FROM  sivel2_gen_categoria AS c JOIN sivel2_gen_supracategoria AS s ON 
-    c.supracategoria_id=s.id WHERE id_tviolencia='C');
-	DROP VIEW IF EXISTS nobelicas;
-	CREATE VIEW nobelicas AS (SELECT id_caso FROM sivel2_gen_acto) UNION 
-    (SELECT id_caso FROM sivel2_gen_actocolectivo) UNION 
-    (SELECT id_caso_presponsable/10000 FROM sivel2_gen_caso_categoria_presponsable) ORDER BY 1;
+require_relative 'auxiliar_eliminar'
 
-  DELETE FROM sivel2_gen_caso_categoria_presponsable WHERE 
-    id_caso_presponsable IN 
-      (SELECT DISTINCT id from sivel2_gen_caso_presponsable WHERE
-      id_caso NOT IN (SELECT id_caso FROM nobelicas ORDER BY 1));
-  UPDATE sivel2_gen_caso SET ubicacion_id=NULL WHERE
-    id NOT IN (SELECT DISTINCT id_caso FROM nobelicas ORDER BY 1);
-  DELETE FROM sivel2_gen_antecedente_victima WHERE
-    id_victima IN (SELECT id FROM sivel2_gen_victima WHERE
-      id_caso NOT IN (SELECT DISTINCT id_caso FROM nobelicas ORDER BY 1));
+lb = ActiveRecord::Base.connection.execute <<-SQL
+  DROP VIEW IF EXISTS belicas;
+  CREATE VIEW belicas AS (
+    SELECT DISTINCT cp.id_caso
+    FROM sivel2_gen_caso_categoria_presponsable AS ccp
+    JOIN sivel2_gen_caso_presponsable AS cp ON cp.id=ccp.id_caso_presponsable
+    WHERE ccp.id_categoria IN (
+      SELECT c.id FROM sivel2_gen_categoria AS c 
+      JOIN sivel2_gen_supracategoria AS s ON s.id=c.supracategoria_id 
+      WHERE id_tviolencia='C'
+    )
+    ORDER BY 1
+  );
+  -- No podemos usar eliminar_casos("SELECT id_caso FROM belicas")
+  -- porque esa funciona elimina antes sivel2_gen_caso_categoria_presponsable
+  -- que deja vacía la consulta belicas
+  SELECT id_caso FROM belicas;
 SQL
-	puts ActiveRecord::Base.connection.execute("SELECT COUNT(*) FROM nobelicas")
 
- ['sivel2_gen_victimacolectiva_vinculoestado', 
-  'sivel2_gen_antecedente_victimacolectiva',
-  'sivel2_gen_profesion_victimacolectiva', 
-  'sivel2_gen_etnia_victimacolectiva', 
-  'sivel2_gen_filiacion_victimacolectiva', 
-  'sivel2_gen_organizacion_victimacolectiva', 
-  'sivel2_gen_rangoedad_victimacolectiva', 
-  'sivel2_gen_sectorsocial_victimacolectiva'].each do |nt|
-	  ActiveRecord::Base.connection.execute(
-      "DELETE FROM #{nt} WHERE victimacolectiva_id IN
-      (SELECT id FROM sivel2_gen_victimacolectiva WHERE
-        id_caso NOT IN 
-        (SELECT DISTINCT id_caso FROM nobelicas ORDER BY 1))")
-  end
-
-	['sivel2_gen_caso_contexto', 
-  'sivel2_gen_caso_presponsable', 'sivel2_gen_antecedente_caso', 
-  'sip_ubicacion', 'sivel2_gen_caso_usuario', 'sivel2_gen_acto', 
-  'sivel2_gen_victima', 
-  'sivel2_gen_caso_region', 'sivel2_gen_caso_frontera', 
-   'sivel2_gen_actocolectivo', 
-  'sivel2_gen_victimacolectiva', 
-  'sivel2_gen_caso_etiqueta', 
-  'sivel2_gen_caso_fuenteprensa', 
-  'sivel2_gen_caso_fotra', 
-  'sivel2_gen_anexo_caso'].each do |nt|
-    puts nt
-	  ActiveRecord::Base.connection.execute(
-      "DELETE FROM #{nt} WHERE id_caso NOT IN " \
-      "(SELECT DISTINCT id_caso FROM nobelicas ORDER BY 1);")
-  end
-	ActiveRecord::Base.connection.execute(
-    "DELETE FROM sivel2_gen_caso_respuestafor WHERE caso_id NOT IN " \
-    "(SELECT DISTINCT id_caso FROM nobelicas ORDER BY 1);")
-	ActiveRecord::Base.connection.execute(
-    "DELETE FROM sivel2_gen_caso WHERE id NOT IN " \
-    "(SELECT DISTINCT id_caso FROM nobelicas ORDER BY 1);")
-	ActiveRecord::Base.connection.execute(
-    "DELETE FROM sip_anexo WHERE id NOT IN " \
-    "(SELECT DISTINCT id_anexo FROM sivel2_gen_anexo_caso);")
+lpore = lb.pluck('id_caso')
+eliminar_casos(
+  "SELECT id FROM sivel2_gen_caso WHERE id IN (#{lpore.join(', ')})"
+)
--- scripts/elimina_datos_privados.rb
+++ scripts/elimina_datos_privados.rb
@@ -24,7 +24,7 @@ ActiveRecord::Base.connection.execute <<-SQL
   UPDATE usuario SET nusuario='adminexp' WHERE id=1;
   INSERT INTO usuario (id, nusuario, fechacreacion)
     SELECT 1, 'adminexp', '2020-05-28'
-      WHERE NOT EXISTS (SELECT 1 FROM usuario WHERE id=1);
+      WHERE 1 NOT IN (SELECT 1 FROM usuario WHERE id=1);
 
   DELETE FROM sip_grupo_usuario WHERE usuario_id<>1;
   UPDATE sivel2_gen_caso_etiqueta SET id_usuario=1;
