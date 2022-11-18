5c2f370c738961439a42d600e30b6a4ba2ed6ccf
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-11-18 05:51:23 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -257,21 +257,25 @@ CREATE FUNCTION public.rand() RETURNS double precision
 CREATE FUNCTION public.sip_edad_de_fechanac_fecharef(anionac integer, mesnac integer, dianac integer, anioref integer, mesref integer, diaref integer) RETURNS integer
     LANGUAGE sql IMMUTABLE
     AS $$
-        SELECT CASE 
-          WHEN anionac IS NULL THEN NULL
-          WHEN anioref IS NULL THEN NULL
-          WHEN mesnac IS NULL OR dianac IS NULL OR mesref IS NULL OR diaref IS NULL THEN 
-            anioref-anionac 
-          WHEN mesnac < mesref THEN
-            anioref-anionac
-          WHEN mesnac > mesref THEN
-            anioref-anionac-1
-          WHEN dianac > diaref THEN
-            anioref-anionac-1
-          ELSE 
-            anioref-anionac
-        END 
-      $$;
+            SELECT CASE 
+              WHEN anionac IS NULL THEN NULL
+              WHEN anioref IS NULL THEN NULL
+              WHEN anioref < anionac THEN -1
+              WHEN mesnac IS NOT NULL AND mesnac > 0 
+                AND mesref IS NOT NULL AND mesref > 0 
+                AND mesnac >= mesref THEN
+                CASE 
+                  WHEN mesnac > mesref OR (dianac IS NOT NULL 
+                    AND dianac > 0 AND diaref IS NOT NULL 
+                    AND diaref > 0 AND dianac > diaref) THEN 
+                    anioref-anionac-1
+                  ELSE 
+                    anioref-anionac
+                END
+              ELSE
+                anioref-anionac
+            END 
+          $$;
 
 
 --
@@ -8646,6 +8650,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20220822132754'),
 ('20221005165307'),
 ('20221102144613'),
-('20221102145906');
+('20221102145906'),
+('20221118032223');
 
 
