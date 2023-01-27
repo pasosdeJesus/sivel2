c6055d43db3f3b3a39093f9a60ece35e303c0871
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-01-27 12:51:48 -0500

    Actualiza

--- config/initializers/msip.rb
+++ config/initializers/msip.rb
@@ -1,9 +1,9 @@
 require 'sivel2_gen/version'
 
 Msip.setup do |config|
-  config.ruta_anexos = ENV.fetch('SIP_RUTA_ANEXOS', 
+  config.ruta_anexos = ENV.fetch('MSIP_RUTA_ANEXOS', 
                                  "#{Rails.root}/archivos/anexos/")
-  config.ruta_volcados = ENV.fetch('SIP_RUTA_VOLCADOS',
+  config.ruta_volcados = ENV.fetch('MSIP_RUTA_VOLCADOS',
                                    "#{Rails.root}/archivos/bd/")
   # En heroku los anexos son super-temporales
   if ENV["HEROKU_POSTGRESQL_MAUVE_URL"]
