e3f3db86e6ba56c637d816ceeb3d80249797ddbd
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-02-09 09:25:23 -0500

    ruta relativa

--- config/initializers/punto_montaje.rb
+++ config/initializers/punto_montaje.rb
@@ -2,4 +2,4 @@ Sivel2::Application.config.relative_url_root = ENV.fetch(
   'RUTA_RELATIVA', '/sivel2')
 Sivel2::Application.config.assets.prefix = ENV.fetch(
   'RUTA_RELATIVA', '/sivel2') == '/' ? 
- '/assets' : (ENV.fetch('RUTA_RELATIVA', '/sivel2') + '/assets')
+ '/assets' : (ENV.fetch('RUTA_RELATIVA', '/sivel2_2') + '/assets')
