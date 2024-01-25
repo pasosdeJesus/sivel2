de6507fbd373198a8db745dec7d79ae3fa51334c
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-01-25 13:26:05 -0500

    Action Cable activo para evitar problema de unicorn

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
 require "action_view/railtie"
-# require "action_cable/engine"
+require "action_cable/engine"
 require "rails/test_unit/railtie"
 
 
