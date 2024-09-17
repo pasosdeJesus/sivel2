6792c6c2cbf384bf9947c34fb258e565451aea71
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-09-17 12:16:18 -0500

    Licencia en acerca de. Avanza https://gitlab.com/pasosdeJesus/sivel2_gen/-/issues/702

--- config/initializers/msip.rb
+++ config/initializers/msip.rb
@@ -12,9 +12,11 @@ Msip.setup do |config|
   config.titulo = ENV.fetch('MSIP_TITULO', "SIVeL").dup.force_encoding('UTF-8') + " #{Sivel2Gen::VERSION}"
 
   config.descripcion = "Sistema de Información de Violencia Política en Línea"
-  config.codigofuente = "https://gitlab.com/pasosdeJesus/sivel2"
-  config.urlcontribuyentes = "https://gitlab.com/pasosdeJesus/sivel2_gen/-/graphs/main"
-  config.urlcreditos = "https://gitlab.com/pasosdeJesus/sivel2/-/blob/main/CREDITOS.md"
+
+  config.codigofuente = "https://gitlab.com/pasosdeJesus/sivel2/-/tree/v2.2"
+  config.urlcontribuyentes = "https://gitlab.com/pasosdeJesus/sivel2/-/graphs/v2.2"
+  config.urllicencia = "https://gitlab.com/pasosdeJesus/sivel2/-/blob/v2.2/LICENCIA.md"
+  config.urlcreditos = "https://gitlab.com/pasosdeJesus/sivel2/-/blob/v2.2/CREDITOS.md"
   config.agradecimientoDios = "<p>
 El mayor agradecimiento al Dios trino, el de la Biblia, a quien dedicamos 
 este trabajo y a quien oramos para que no sea usado por estructuras armadas
