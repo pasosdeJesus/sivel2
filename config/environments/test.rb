5b1da0216b2b1df31e55d2597297c1d1d7a30251
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-12-18 10:11:39 -0500

    Actualiza

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,6 +8,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+  config.assets.css_compressor = nil
+
   # While tests run files are not watched, reloading is not necessary.
   config.enable_reloading = false
 
