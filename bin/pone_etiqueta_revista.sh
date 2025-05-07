a1f698b433b9709c0c2a7560ad3e5876db0f572a
Nombre Usuario <correo@usuario.net>
2025-05-07 03:33:54 -0500

    Mejora script que pone etiqueta. Avanza #790

--- bin/pone_etiqueta_revista.sh
+++ bin/pone_etiqueta_revista.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo "Pone etiqueta de revista a casos de un periodo"
-echo "Usar solo para último perido no para periodos históricos --que podrían
+echo "Usar solo para último periodo no para periodos históricos --que podrían
 tener casos actualizados que no deben marcarse con la etiqueta de la revista"
 fi="$1"
 if (test "$fi" = "") then {
-	echo "Falta fecha inical como primer parámetro" 
+	echo "Falta fecha inicial como primer parámetro" 
 	exit 1;
 } fi;
 ff="$2"
@@ -20,7 +20,7 @@ if (test "$idet" = "") then {
 } fi;
 idus="$4"
 if (test "$idus" = "") then {
-	echo "Falta id. de usuario que pondrá la etiequeta como cuarto parámetro" 
+	echo "Falta id. de usuario que pondrá la etiqueta como cuarto parámetro" 
 	exit 1;
 } fi;
 
@@ -29,17 +29,44 @@ if (test "$idus" = "") then {
 . .env
 
 
-echo "Etiquetas por agregar"
-psql -U $BD_USUARIO -h /var/www/var/run/postgresql $BD_PRO -c "SELECT
-id AS id_caso, $idet AS id_etiqueta, $idus AS id_usuario, NOW() AS fecha,
-NOW() AS created_at, now() AS updated_at FROM sivel2_gen_caso 
-WHERE fecha>='2024-01-01' AND fecha<='2024-06-30';"
+echo "Etiquetas por agregar (en casos no-belicas a los que les faltan)"
+cons="
+SELECT id AS caso_id, 
+  $idet AS etiqueta_id, 
+  $idus AS usuario_id, 
+  NOW() AS fecha,
+  NOW() AS created_at, 
+  NOW() AS updated_at 
+FROM sivel2_gen_caso 
+WHERE 
+  fecha>='$fi' AND fecha<='$ff' AND 
+  id NOT IN (
+    SELECT caso_id 
+    FROM sivel2_gen_caso_presponsable AS cp 
+    WHERE cp.id IN (
+      SELECT caso_presponsable_id 
+      FROM sivel2_gen_caso_categoria_presponsable 
+      WHERE categoria_id IN (
+        SELECT cat.id 
+        FROM sivel2_gen_categoria AS cat 
+        JOIN sivel2_gen_supracategoria AS sup ON cat.supracategoria_id=sup.id 
+        WHERE sup.tviolencia_id='C'
+      )
+    )
+  ) AND 
+  id NOT IN (
+    SELECT caso_id 
+    FROM sivel2_gen_caso_etiqueta
+    WHERE etiqueta_id=$idet
+  )
+"
+echo "Consulta base=$cons"
+
+echo "Enter para ejecutarla" 
+read
+psql -U $BD_USUARIO -h /var/www/var/run/postgresql $BD_PRO -c "$cons"
+
 echo "Enter para agregarlas" 
 read
-psql -U $BD_USUARIO -h /var/www/var/run/postgresql $BD_PRO -c "INSERT INTO
-sivel2_gen_caso_etiqueta
-(SELECT id AS id_caso, $idet AS id_etiqueta, $idus AS id_usuario, NOW() AS fecha,
-NOW() AS created_at, now() AS updated_at FROM sivel2_gen_caso 
-WHERE fecha>='2024-01-01' AND fecha<='2024-06-30');"
+psql -U $BD_USUARIO -h /var/www/var/run/postgresql $BD_PRO -c "INSERT INTO sivel2_gen_caso_etiqueta ($cons);"
 
-#psql -U $BD_USUARIO -h /var/www/var/run/postgresql $BD_PRO -c "DELETE FROM sivel2_gen_caso WHERE fecha>='$f' AND TRIM(memo)='';"
