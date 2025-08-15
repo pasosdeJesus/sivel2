d76cfeb10f661589452aa65b24af3a752da6428f
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-08-15 05:31:28 -0500

    No usa propshaft

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,2 @@
 //= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_tree ../stylesheets
-//= link application.css
-//= link recursos_sprockets.js
-//= link recursos_sprockets.js.map
 //= link_tree ../builds
